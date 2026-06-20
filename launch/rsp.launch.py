e8580b3c96a9e0bb4a5755af1c0435116cdd10f7
Kriti Paudel <kriti13p@gmail.com>
2026-06-20 16:12:09 +0545

    Add wheels, caster wheel, collision, and inertial properties

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -1,20 +1,13 @@
 import os
-
 from ament_index_python.packages import get_package_share_directory
-
 from launch import LaunchDescription
 from launch.substitutions import LaunchConfiguration
 from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
-
 import xacro
-
-
 def generate_launch_description():
-
     # Check if we're told to use sim time
     use_sim_time = LaunchConfiguration('use_sim_time')
-
     # Process the URDF file
     pkg_path = os.path.join(get_package_share_directory('my_bot'))
     xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro')
@@ -29,6 +22,11 @@ def generate_launch_description():
         parameters=[params]
     )
 
+    # Create a joint_state_publisher_gui node
+    node_joint_state_publisher_gui = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui'
+    )
 
     # Launch!
     return LaunchDescription([
@@ -36,6 +34,6 @@ def generate_launch_description():
             'use_sim_time',
             default_value='false',
             description='Use sim time if true'),
-
-        node_robot_state_publisher
+        node_robot_state_publisher,
+        node_joint_state_publisher_gui
     ])
